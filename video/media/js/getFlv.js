a345bcc4da05c43ee2709686f5aef4e247502442
Shinren Pan <shinren.pan@gmail.com>
2014-12-25 21:43:54 +0800

    Update getFlv.js

--- video/media/js/getFlv.js
+++ video/media/js/getFlv.js
@@ -1,14 +1,19 @@
 // 不需要jQuery
-var button_label0='獲取地址';
-var button_label1='正在獲取';
-var result_label0='等待提交...';
-var result_label1='<img src="/assets/loader.gif" alt="loading" /> 獲取中(專輯需時稍長,不要關閉本頁)...';
-var input_placeholder='在這裡粘貼視頻頁面的地址...(包含http://)';
-var msg = "請用右鍵“目標另存為”或用快車下載\n【提示：大部分視頻無法使用迅雷下載】";
-var msg_filenamecopyed = "已複製文件名: ";
+var button_label0 = '获取视频';
+var button_label1 = '正在获取';
+var result_label0 = '等待提交...';
+var result_label1 = '<div class="progress progress-striped active"><div class="bar" style="width: 100%;">loading...</div></div>';
+var input_placeholder = '在这里粘贴视频页面的地址...(包含http://)';
+var msg = "请用右键“目标另存为”或用快车下载\n【提示：大部分视频无法使用迅雷下载】";
+var msg_filenamecopyed = "已复制文件名: ";
+var msg_copyed = '已复制到剪切板';
+var msg_xml = 'XML格式的输出所有视频，目前免费';
+var msg_ckxml = 'xml格式for ckplayer，只显示第一个清晰度';
+
+if(typeof baseurl == 'undefined')  baseurl = 'https://www.flvxz.com';
 
 String.prototype.trim= function(){  
-    // 用正則表達式將前後空格  
+    // 用正则表达式将前后空格  
     // 用空字符串替代。  
     return this.replace(/(^\s*)|(\s*$)/g, "");  
 }
@@ -31,11 +36,62 @@ String.prototype.setKeyValue = function(key,value){
         }
     }
 }
-function urlencode(url){
-	return encodeBase64(url.replace('http://','http:##').replace('https://','https:##'));
+function isIE(){
+    var b = document.createElement('b')
+    b.innerHTML = '<!--[if IE]><i></i><![endif]-->'
+    return b.getElementsByTagName('i').length === 1
 }
-function copyToClipboard()
-{ 
+
+// 需要jQuery
+(function ($) {
+
+$(document).ready(function(){
+	var supportPlaceholder = 'placeholder' in document.createElement('input');
+	if(supportPlaceholder){
+		// 设置placeholder属性
+		$('#videoPageURI').attr('placeholder',input_placeholder);
+	}
+	else{
+		// 用JS实现placeholder
+		$('#videoPageURI').focus(function(){this.select();if(this.value==input_placeholder){this.value='';}});
+		$('#videoPageURI').blur(function(){if(this.value==''){this.value=input_placeholder}});
+	}
+	$('#videoPageURI').change(function(){
+		var videoPageURI = $('#videoPageURI').val();
+		$('#videoPageURI').val(videoPageURI.trim());
+		if(videoPageURI == '' || /^https?:/.test(videoPageURI)){
+			$('#videoPageURI').parent().removeClass('control-group error');
+			$('#fetchButton').attr('disabled',false);
+		}
+		else if(/^https?:/.test(urldecode(videoPageURI))){
+			$('#videoPageURI').val(urldecode(videoPageURI));
+			$('#videoPageURI').parent().removeClass('control-group error');
+			$('#fetchButton').attr('disabled',false);
+		}
+		else{
+			$('#videoPageURI').parent().addClass('control-group error');
+			$('#fetchButton').attr('disabled',"disabled");
+		}
+	});
+	//根据url的地址，执行init
+	var videoPageURI = location.toString().queryString('url');
+	$('#fetchButton').val(button_label0);
+	if(videoPageURI==null){
+		if(!supportPlaceholder) $('#videoPageURI').val(input_placeholder);
+		$('#resultbox').hide();
+	} else {
+		if(videoPageURI.indexOf('http')===0){
+			videoPageURI = urldecode(decodeURIComponent(videoPageURI));
+		} else {
+			videoPageURI = urldecode(videoPageURI);
+			if(videoPageURI.indexOf('http')!==0 && videoPageURI.indexOf(':')===0) videoPageURI='http'+videoPageURI;
+		}
+		$('#videoPageURI').val(videoPageURI);
+		getFlv();
+	}
+	$('#examplebox').show();
+});
+window.copyToClipboard = function(){ 
 	if(window.clipboardData){ 
 		window.clipboardData.clearData(); 
 		var btn = $(this);
@@ -48,74 +104,22 @@ function copyToClipboard()
 		clip.on('complete', function(client, args) {
 			var btn = $(this);
 			var oldhtml = btn.html();
-			$(this).html('已複製到剪切板').attr('disabled','disabled');
+			$(this).html(msg_copyed).attr('disabled','disabled');
 			setTimeout(function(){btn.html(oldhtml).attr('disabled',false);},1000);
 		});
 		return false;
 	}
 }
-function norefererDownload(furl) {
-	if(furl.indexOf('http://')==0){
-		furl = furl.substring(7);
-		window.location = "https://noreferer.flvxz.com/"+furl;
-	}
-	//window.location = "https://noreferer.flvxz.com/?url="+urlencode(furl);
-	//var winobj=window.open(furl,"down");
-	//if(winobj==null) window.location=furl;
-	return false;
-}
-
-// 需要jQuery
-(function ($) {
-
-$(document).ready(function(){
-	var supportPlaceholder = 'placeholder' in document.createElement('input');
-	if(supportPlaceholder){
-		// 設置placeholder屬性
-		$('#videoPageURI').attr('placeholder',input_placeholder);
-	}
-	else{
-		// 用JS實現placeholder
-		$('#videoPageURI').focus(function(){this.select();if(this.value==input_placeholder){this.value='';}});
-		$('#videoPageURI').blur(function(){if(this.value==''){this.value=input_placeholder}});
-	}
-	$('#videoPageURI').change(function(){
-		$('#videoPageURI').val($('#videoPageURI').val().trim());
-		if($('#videoPageURI').val() == '' || /^https?:/.test($('#videoPageURI').val())){
-			$('#videoPageURI').parent().removeClass('control-group error');
-			$('#fetchButton').attr('disabled',false);
-			//$('#fetchButton').removeAttr('disabled');
-		}
-		else{
-			$('#videoPageURI').parent().addClass('control-group error');
-			$('#fetchButton').attr('disabled',"disabled");
-		}
-	});
-	//根據url的地址，執行init
-	var videoPageURI = location.toString().queryString('url');
-	$('#fetchButton').val(button_label0);
-	if(videoPageURI==null){
-		if(!supportPlaceholder) $('#videoPageURI').val(input_placeholder);
-		$('#resultbox').hide();
-	} else {
-		if(videoPageURI.indexOf('http')===0){
-			videoPageURI = decodeUri(decodeURIComponent(videoPageURI));
-		} else {
-			videoPageURI = decodeUri(videoPageURI);
-			if(videoPageURI.indexOf('http')!==0 && videoPageURI.indexOf(':')===0) videoPageURI='http'+videoPageURI;
-		}
-		$('#videoPageURI').val(videoPageURI);
-		getFlv();
-	}
-	$('#examplebox').show();
-});
 window.notify_finish = function(){
+	$('a[rel="noreferrer"]').click(function(){
+		if(isIE()) {var winobj=window.open($(this).attr('href'),"down");if(winobj==null) window.location=$(this).attr('href'); return false;}
+	});
 	$('.copyclipboard').each(function(){
 		var clip = new ZeroClipboard(this);
 		clip.on('complete', function(client, args) {
 			var btn = $(this);
 			var oldhtml = btn.html();
-			$(this).html('已複製到剪切板').attr('disabled','disabled');
+			$(this).html(msg_copyed).attr('disabled','disabled');
 			setTimeout(function(){btn.html(oldhtml).attr('disabled',false);},1000);
 		});
 	});
@@ -127,11 +131,14 @@ window.getFlv = function() {
   $('#fetchButton').val(button_label1);
   $('#result').css('display','block');
   $('#result').html(result_label1);
+  $('#visitoriginal').attr('href',uri);
   $('#resultbox').show();
-	$.getScript("http://www.flvxz.com/getFlv.php?url="+encodeUri(uri), function(){
+  var encodedUri = urlencode(uri);
+  var scriptUrl = baseurl + "/getFlv.php?url=" + encodedUri;
+  //if($('#ggad').css('height')=='0px') scriptUrl = 'http://www.flvxz.com/js/adblocked.js';
+	$.getScript(scriptUrl, function(){
 		$('#fetchButton').attr('disabled',false);
 		$('#fetchButton').val(button_label0);
-		$('#apiurls').html('<a class="btn btn-mini" href="http://api.flvxz.com/url/'+encodeUri(uri)+'" target="_blank" title="xml格式的輸出，目前免費">xml</a><a class="btn btn-mini" href="http://api.flvxz.com/url/'+encodeUri(uri)+'/xmlformat/ckxml" target="_blank" title="xml格式for ckplayer">ckxml</a>');
 	});
 	//$.getScript("http://log.flvxz.com/?url="+urlencode(uri));
 }
@@ -143,29 +150,35 @@ window.flvout = function(d,resultid){
 		r.html(r.html()+d);
 	}
 	else{
-		var tip = '結果稍後出現...'; // 不能包含loading，因為#result判斷用了loading
+		var tip = '......'; // 不能包含loading，因为#result判断用了loading
 		var ri = $('#'+resultid);
 		if(!ri[0]) r.html(r.html()+'<div id=\"'+resultid+'\">'+tip+'</div>');
-		ri = $('#'+resultid); // 添加了resultid的div，重新獲取
-		if(ri.html().indexOf(tip)>=0) ri.html('');
+		ri = $('#'+resultid); // 添加了resultid的div，重新获取
+		if(ri.html()==tip) ri.html('');
 		ri.children('.flvxz_tobereplaced').remove();
 		ri.html(ri.html()+d);
 	}
 }
-window.encodeUri = function(uri){
-	uri = uri.replace(/^(http:\/\/[^\/]*(?:youku|tudou|ku6|youtube|yinyuetai|letv|sohu))xia.com\//,'$1.com/');
+window.urlencode = function(uri){
+	uri = uri.replace(/^(http:\/\/[^\/]*(?:youku|tudou|ku6|yinyuetai|letv|sohu|youtube|iqiyi|facebook|vimeo|cutv|cctv|pptv))xia.com\//,'$1.com/');
+	uri = uri.replace(/^(http:\/\/[^\/]*(?:bilibili|acfun|pps))xia\.tv\//,'$1.tv/');
 	uri = uri.replace(/^(https?:)\/\//,'$1##');
-	uri = encodeBase64(uri);
+	uri = $.base64.btoa(uri);
+	uri = uri.replace('+','-').replace('/','_');
 	return uri;
 }
-window.decodeUri = function(uri){
-	if(/^[A-Za-z0-9=\+\/]+#*$/.test(uri)) uri = decodeBase64(uri);
+window.urldecode = function(uri){
+	var sharpPos;
+	sharpPos = uri.indexOf('#');
+	if(sharpPos>0) uri = uri.substring(0,sharpPos);
+	if(/^[A-Za-z0-9=\+\/]+$/.test(uri)) uri = $.base64.atob(uri);
+	else if(/^[A-Za-z0-9=\-_]+$/.test(uri)) uri = $.base64.atob(uri.replace('-','+').replace('_','/'));
 	uri = uri.replace(/^(https?:)##/,'$1//');
 	return uri;
 }
 window.parseFlv = function(){
 	var uri=$('#videoPageURI').val().trim();
-	uri = encodeUri(uri);
+	uri = urlencode(uri);
 	top.location = '?url=' + uri;
 }
 
@@ -178,45 +191,35 @@ window.in_array = function(stringToSearch, arrayToSearch) {
     }
     return false;
 }
-
-window.preview = function(sharecode){
-	$('#previewbox').html(sharecode);
-	$('#previewbox').slideDown('slow');
-}
-
 window.youkupass = function(this_btn,vid){
 	var pass = $(this_btn).prev().val();
 	if(pass.length==0) return;
-	$(this_btn).val('正在驗證密碼').attr('disabled', 'disabled');
-	$.getScript("http://www.flvxz.com/youkupass.php?vid=" + vid + "&password=" + pass);	
+	$(this_btn).val('正在验证密码').attr('disabled', 'disabled');
+	$.getScript(baseurl + "/youkupass.php?vid=" + vid + "&password=" + pass);	
 }
 window.youkulogin = function(this_btn,vid){
 	var pass = $(this_btn).prev().val();
 	var user = $(this_btn).prev().prev().val();
 	if(user.length==0||pass.length==0) return;
-	$(this_btn).val('正在驗證密碼').attr('disabled', 'disabled');
-	$.getScript("http://www.flvxz.com/youkulogin.php?vid=" + vid + "&username=" + user + "&password=" + pass);	
+	$(this_btn).val('正在验证密码').attr('disabled', 'disabled');
+	$.getScript(baseurl + "/youkulogin.php?vid=" + vid + "&username=" + user + "&password=" + pass);	
 }
 window.tudoupass = function(this_btn,vid){
 	var pass = $(this_btn).prev().val();
 	if(pass.length==0) return;
-	$(this_btn).val('正在驗證密碼').attr('disabled', 'disabled');
-	$.getScript("http://www.flvxz.com/tudoupass.php?vid=" + vid + "&password=" + pass);	
+	$(this_btn).val('正在验证密码').attr('disabled', 'disabled');
+	$.getScript(baseurl + "/tudoupass.php?vid=" + vid + "&password=" + pass);	
 }
 window.v56pass = function(this_btn,vid,sid){
 	var pass = $(this_btn).prev().val();
 	if(pass.length == 0) return;
-	$(this_btn).val('正在驗證密碼').attr('disabled', 'disabled');
-	$.getScript("http://www.flvxz.com/getFlv.php?url=" + encodeBase64('http://www.56.com/fakeurl/vid/' + vid + "/sid/" + sid + "/password/" + pass));
+	$(this_btn).val('正在验证密码').attr('disabled', 'disabled');
+	$.getScript(baseurl + "/getFlv.php?url=" + urlencode('http://www.56.com/fakeurl/vid/' + vid + "/sid/" + sid + "/password/" + pass));
 }
 window.kankan = function(vid,title){
 	var arr = vid.split('_');
 	$('#result_'+arr[2]).html('<span class="flvxz_tobereplaced">正在解析 '+title+'</span>');
-	$.getScript("http://www.flvxz.com/kankan.php?vid=" + vid + '&title=' + encodeURIComponent(title) );	
-}
-window.funshion = function(vid,title){
-	$('#result_'+vid).html('<span class="flvxz_tobereplaced">正在解析 '+title+'</span>');
-	$.getScript("http://www.flvxz.com/getFlv.php?url="+encodeBase64('http://www.funshion.com/fakeurl/'+encodeURIComponent(title)+'/cid/'+vid));
+	$.getScript(baseurl + "/kankan.php?vid=" + vid + '&title=' + encodeURIComponent(title) );	
 }
 
 }(jQuery));
